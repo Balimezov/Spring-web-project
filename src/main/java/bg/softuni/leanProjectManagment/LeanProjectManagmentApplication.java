8b5dd2cc1cafb9e9a6bc60f0cac884ba6b08fade
Nikola <nikbalimezov@gmail.com>
2024-12-27 21:26:25 +0200

    initialize

---
+++ src/main/java/bg/softuni/leanProjectManagment/LeanProjectManagmentApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.leanProjectManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanProjectManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanProjectManagmentApplication.class, args);
+	}
+
+}
